6f424a5aa8f0887acb182d9193893906606cb54b
Ysuyuki Fujii <is0232xf@gmail.com>
2016-09-09 14:40:11 +0900

    推定結果をグラフで表示するように変更した

--- particle_filter.py
+++ particle_filter.py
@@ -19,23 +19,6 @@ p_s_a = [[[1, 0, 0, 0, 0], [0.9, 0.1, 0, 0, 0], [0, 0.9, 0.1, 0, 0], [0, 0, 0.9,
          [[0.1, 0.9, 0, 0, 0], [0, 0.1, 0.9, 0, 0], [0, 0, 0.1, 0.9, 0], [0, 0, 0, 0.1, 0.9],  [0, 0, 0, 0, 1]],
          [[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]]
 
-p_s_o = [[0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.85],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.85, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.85, 0.85, 0.85, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01],
-         [0.01, 0.01, 0.01, 0.01, 0.01]]
-
 
 class ParticleFilter(object):
     def __init__(self):
@@ -45,13 +28,13 @@ class ParticleFilter(object):
     def update_p_s_bar(self, particle, a):
         for i in range(len(particle)):
             s = particle[i]
-            p_s = bayes_filter.p_s_a[a][s]
+            p_s = self.p_s_a[a][s]
             new_s = bayes_filter.multinomial(p_s)
             particle[i] = new_s
         return particle
 
-    def add_weight(self, particle, particle_num, o):
-        self.p_s_o = p_s_o
+    def update_p_s(self, particle, particle_num, o):
+        self.p_o_s = p_o_s
         w_particle = 5 * [0]
         particle_counter = 5 * [0]
         new_w_particle = 5 * [0]
@@ -61,19 +44,17 @@ class ParticleFilter(object):
         print "particle_counter:", particle_counter
 
         for i in range(len(particle_counter)):
-            w_particle[i] = particle_counter[i] * p_s_o[o][i]
+            w_particle[i] = particle_counter[i] * p_o_s[i][o]
         print "w_particle:", w_particle
 
         sum_w = sum(w_particle[i] for i in range(len(w_particle)))
 
         for i in range(len(w_particle)):
             new_w_particle[i] = (w_particle[i] / sum_w)
-        print "new_w_particle:", new_w_particle
-        return new_w_particle
+        bayes_filter.show_p_s(new_w_particle)
 
-    def update_p_s(self, particle, w_particle, particle_num):
         for i in range(particle_num):
-            particle[i] = bayes_filter.multinomial(w_particle)
+            particle[i] = bayes_filter.multinomial(new_w_particle)
         return particle
 
 
@@ -100,7 +81,22 @@ class Controller(object):
         return bayes_filter.is_empty(self.goals)
 
 
+def calculate_ratio_of_particle(particle):
+        particle_counter = 5 * [0]
+        particle_ratio = 5 * [0]
+
+        for i in range(len(particle_counter)):
+            particle_counter[i] = particle.count(i)
+        for i in range(len(particle_counter)):
+            particle_ratio[i] = particle_counter[i] / float(len(particle))
+        return particle_ratio
+
 if __name__ == "__main__":
+    o_log = []
+    determined_s_log = []
+    a_log = []
+    actual_s_log = []
+    actual_s_log.append(0)
     estimater = ParticleFilter()
     simulator = bayes_filter.Simulator(p_s_a, p_o_s)
     goals = [4, 0]
@@ -114,24 +110,29 @@ if __name__ == "__main__":
     while True:
         print "step:", t, "##########################"
         o = simulator.get_o()
+        o_log.append(o)
         print "o =", o
-        w_particle = estimater.add_weight(particle, particle_num, o)
-        particle = estimater.update_p_s(particle, w_particle, particle_num)
-        print "after observe:", particle
+        particle = estimater.update_p_s(particle, particle_num, o)
         print collections.Counter(particle).most_common(1)
         determined_s = collections.Counter(particle).most_common(1)[0][0]
+        determined_s_log.append(determined_s)
 
         a = controller.determine_a(determined_s)
+        a_log.append(a)
         print "a =", a
 
         if controller.is_terminated() is True:
-#            print_result(o_log, actual_s_log, determined_s_log, a_log, t)
+            bayes_filter.print_result(o_log, actual_s_log,
+                                      determined_s_log, a_log, t)
             print "Finish"
             break
 
         simulator.set_a(a)
         s = simulator.get_s()
+        actual_s_log.append(s)
         print "s =", s
         t = t + 1
         particle = estimater.update_p_s_bar(particle, a)
+        particle_ratio = calculate_ratio_of_particle(particle)
+        bayes_filter.show_p_s(particle_ratio)
         print "before observe:", particle
