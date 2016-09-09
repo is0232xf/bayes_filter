f0e0b84e523b17467a2730a7c81ac52ca0cfae03
Ysuyuki Fujii <is0232xf@gmail.com>
2016-09-09 19:19:16 +0900

    実行結果をグラフで表示する

--- simulator.py
+++ simulator.py
@@ -6,41 +6,64 @@ Created on Fri Sep 09 16:09:40 2016
 """
 
 import random
+import matplotlib.pyplot as plt
 
 
-class Simulator(object):
-    def __init__(self):
+class Continuous1dSimulator(object):
+    def __init__(self, var_a, var_o):
         self._s = 0
-        self.sigma = 0.1
-        self.landmark = [0, 1, 2, 3, 4]
+        self.std_a = var_a ** 0.5
+        self.std_o = var_o ** 0.5
+        self.landmarks = [0, 1, 2, 3, 4]
 
-    def _draw_s(self, actual_a):
+    def _draw_s(self, a):
+        actual_a = random.gauss(a, self.std_a)
         new_s = self._s + actual_a
         return new_s
 
     def get_o(self):
         o = 5 * [0]
-        for i in range(len(self.landmark)):
-            mu = self.landmark[i] - self._s
-            o[i] = random.gauss(mu, self.sigma)
+        for i in range(len(self.landmarks)):
+            distance = self.landmarks[i] - self._s
+            o[i] = random.gauss(distance, self.std_o)
         return o
 
     def set_a(self, a):
-        actual_a = random.gauss(a, self.sigma)
-        self._s = self._draw_s(actual_a)
+        self._s = self._draw_s(a)
 
     def get_s(self):
         return self._s
 
+def demo_continuous1d_simulator():
+    std_a = 0.1
+    std_o = 0.1
+    var_a = std_a ** 2
+    var_o = std_o ** 2
+    simulator = Continuous1dSimulator(var_a, var_o)
+    actions = [1] * 4 + [-1] * 4
+    s = [simulator.get_s()]
 
-if __name__ == "__main__":
-    simulator = Simulator()
-    a = 1
-
-    for i in range(4):
+    for a in actions:
         simulator.set_a(a)
-        s = simulator.get_s()
+        s.append(simulator.get_s())
         o = simulator.get_o()
 
-        print "s :", s
+        print "s :", s[-1]
         print "o :", o
+
+        plt.ylim([-5.0, 5.0])
+        plt.bar(range(len(o)), o, align='center')
+        plt.grid()
+        plt.show()
+
+    plt.gca().invert_yaxis()
+    plt.xlabel("state")
+    plt.ylabel("time")
+    plt.xlim([-1.0, 5.0])
+    plt.plot(s, range(len(s)), "g--x", markersize=10)
+    plt.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    demo_continuous1d_simulator()
