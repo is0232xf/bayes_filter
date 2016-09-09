7d43d8bd4ab77eb81487e782ef12678235896120
Ysuyuki Fujii <is0232xf@gmail.com>
2016-09-09 19:20:48 +0900

    連続一次元空間用のパーティクルフィルタの実装を開始した

---
+++ continuous1d_particle_filter.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 09 19:08:12 2016
+
+@author: Fujiichang
+"""
+
+
+class Continuous1dControllor(object):
+    def __init__(self, goals):
+        self.goals = goals
+
+    def determine_a():
+        pass
+
+    def is_terminated():
+        pass
+
+
+class Continuous1dParticlefilter(object):
+    def update_p_s():
+        pass
+
+if __name__ == "__main__":
