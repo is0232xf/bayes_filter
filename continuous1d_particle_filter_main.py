d28eb0c68dd875446559c7ee9cc45cb1894be4c5
Ysuyuki Fujii <is0232xf@gmail.com>
2016-09-30 18:00:39 +0900

    結果の表示する関数の内容を一部変更した

--- continuous1d_particle_filter_main.py
+++ continuous1d_particle_filter_main.py
@@ -13,8 +13,9 @@ import continuous1d_simulator
 
 def show_particle_distribution(particle, s, determined_s, title,
                                ylabel="density"):
-    plt.plot(s, 0.2, 'r*', markersize=20)
-    plt.plot(determined_s, 0.2, 'o', markersize=20)
+    plt.plot(determined_s, 0.2, 'o', markersize=20, label="determined_s")
+    plt.plot(s, 0.2, 'r*', markersize=20, label="actual_s")
+    plt.legend(loc='upper right', fontsize=15, numpoints=1)
     plt.grid()
     plt.hist(particle, bins=20, normed=True, histtype='stepfilled')
     plt.title(title)
@@ -25,32 +26,22 @@ def show_particle_distribution(particle, s, determined_s, title,
     plt.show()
 
 
-def show_actual_s__result(s):
+def show_result(s, title, line_type):
     plt.xlim(-1, 5)
     plt.grid()
+    plt.title(title)
     plt.rcParams["font.size"] = 24
     plt.tight_layout()
     plt.gca().invert_yaxis()
     plt.xlabel("state")
     plt.ylabel("time")
-    plt.plot(s, range(len(s)), "g--x", markersize=10)
-    plt.show()
-
-
-def show_determined_s_result(determined_s):
-    plt.xlim(-1, 5)
-    plt.grid()
-    plt.rcParams["font.size"] = 24
-    plt.tight_layout()
-    plt.gca().invert_yaxis()
-    plt.xlabel("state")
-    plt.ylabel("time")
-    plt.plot(determined_s, range(len(determined_s)), "-+", markersize=10)
+    plt.plot(s, range(len(s)), line_type, markersize=10)
     plt.show()
 
 
 def show_merged_result(s, determined_s):
     plt.xlim(-1, 5)
+    plt.title("merged result")
     plt.rcParams["font.size"] = 24
     plt.tight_layout()
     plt.gca().invert_yaxis()
@@ -63,9 +54,9 @@ def show_merged_result(s, determined_s):
     plt.show()
 
 
-def calculate_rms(time, actual_s_log, determined_s_log):
+def calculate_rms(actual_s_log, determined_s_log):
     accidental_error = []
-    for t in range(time):
+    for t in range(len(actual_s_log)):
         accidental_error.append(abs(actual_s_log[t] - determined_s_log[t])**2)
     sum_a = sum(accidental_error)
 #    print sum_a
@@ -77,20 +68,20 @@ if __name__ == "__main__":
     std_o = 0.3
     var_a = std_a ** 2
     var_o = std_o ** 2
-    eps = 0.3
+    allowable_range = 0.3
     o_log = []
     determined_s_log = []
     a_log = []
     actual_s_log = []
     actual_s_log.append(0)
-    estimator = continuous1d_particle_filter.Continuous1dParticlefilter(
-        var_a, var_o)
     simulator = continuous1d_simulator.Continuous1dSimulator(
         var_a, var_o)
+    estimator = continuous1d_particle_filter.Continuous1dParticlefilter(
+        var_a, var_o, simulator.landmarks)
     goals = [4, 0]
-    controller = continuous1d_particle_filter.Continuous1dControllor(goals,
-                                                                     eps)
-    particle_num = 1000
+    controller = continuous1d_particle_filter.Continuous1dControllor(
+        goals, allowable_range)
+    particle_num = 10000
     w_particle = particle_num * [0]
     w_particle[0] = 1
     particle = particle_num * [0]
@@ -126,8 +117,8 @@ if __name__ == "__main__":
         show_particle_distribution(particle, s, determined_s,
                                    "before observation",
                                    "$\overline{bel}(s_t)$")
-    show_actual_s__result(actual_s_log)
-    show_determined_s_result(determined_s_log)
+    show_result(actual_s_log, "actual s", "g--x")
+    show_result(determined_s_log, "determined s", "-+")
     show_merged_result(actual_s_log, determined_s_log)
-    print "RMS =",  calculate_rms(t, actual_s_log, determined_s_log)
+    print "RMS =",  calculate_rms(actual_s_log, determined_s_log)
     print "Finish"
